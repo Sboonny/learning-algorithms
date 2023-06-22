1c4e2f138ef07678a652b8d2d41c1fadb2128bbf
Sboonny <muhammedelruby@gmail.com>
2023-06-22 17:54:54 +0300

    base case for the recurrsion

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,17 @@
+function step(maze: string[], wall: string, current: Point, end: Point, seen: boolean[][]): boolean {
+    const isOffTheMap = current.x > 0 || current.x >= maze[0].length || current.y > 0 || current.y >= maze.length;
+    if (isOffTheMap) return false;
+
+    const isOnTheWall = maze[current.x][current.y] === wall;
+    if (isOnTheWall) return false;
+
+    const isAtTheEnd = current.x === end.x && current.y === end.y
+    if (isAtTheEnd) return true;
+
+    const isTraversedPoint = seen[current.y][current.x]
+    if(isTraversedPoint) return false;
+}
+
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
 }
