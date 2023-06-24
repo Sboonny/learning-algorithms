79a07028ff0aff58d4864eea3e4f89c3a9241a6b
Sboonny <muhammedelruby@gmail.com>
2023-06-24 22:01:17 +0300

    create append for linked list

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -6,10 +6,12 @@ type Node<T> = {
 export default class DoublyLinkedList<T> {
     public length: number;
     private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
         this.length = 0;
         this.head = undefined;
+        this.tail = undefined;
     }
 
     prepend(item: T): void {
@@ -17,7 +19,7 @@ export default class DoublyLinkedList<T> {
 
         this.length++;
         if (!this.head) {
-            this.head = node;
+            this.head = this.tail = node;
             return;
         }
         node.next = this.head;
@@ -28,13 +30,13 @@ export default class DoublyLinkedList<T> {
         if (idx > this.length) {
             throw new Error("index is bigger than array length");
         }
-        this.length++;
         if (idx === this.length) {
             this.append(item);
         } else if (idx === 0) {
             this.prepend(item);
         }
 
+        this.length++;
         let curr = this.head;
         for (let i = 0; curr && i < this.length; ++i) {
             curr = curr.next;
@@ -51,7 +53,19 @@ export default class DoublyLinkedList<T> {
             curr.prev.next = curr;
         }
     }
-    append(item: T): void {}
+    append(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
+
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+    }
     remove(item: T): T | undefined {}
     get(idx: number): T | undefined {}
     removeAt(idx: number): T | undefined {}
