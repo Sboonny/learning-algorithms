d473ed01ce87b5b6675e403be5109e04b237a57b
Sboonny <muhammedelruby@gmail.com>
2023-07-12 20:03:38 +0300

    minHeap half part

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,41 @@
 export default class MinHeap {
     public length: number;
+    private data: number[];
 
-    
+
 
     constructor() {
+        this.data = [];
+        this.length = 0;
     }
 
     insert(value: number): void {
 
-}
+    }
     delete(): number {
 
-}
+    }
+
+    private heapifyUp(idx: number) {
+        if(idx === 0) return;
+
+        const currentParent = this.parent(idx);
+        const parentValue = this.data[currentParent];
+        const currentValue = this.data[idx];
+        if(parentValue > currentValue) {
+            this.data[idx] = parentValue
+            this.data[currentParent] = currentValue
+            this.heapifyUp(currentParent);
+        }
+    }
+
+    private parent(idx: number) {
+        return Math.floor((idx - 1) / 2);
+    }
+    private leftChild(idx: number) {
+        return Math.floor((idx * 2) + 1);
+    }
+    private rightChild(idx: number) {
+        return Math.floor((idx * 2) + 2);
+    }
 }
