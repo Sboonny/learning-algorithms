78a45b7bef80630e116f4f3fbc0176f75c4c265f
Sboonny <muhammedelruby@gmail.com>
2023-08-12 20:35:28 +0300

    implement dijkstra list

---
+++ src/day1/DijkstraList.ts
@@ -0,0 +1,57 @@
+function hasUnvisited(seen: boolean[], distances: number[]): boolean {
+    return seen.some((source, i) => !source && distances[i] < Infinity);
+}
+
+function getLowestUnvisited(seen: boolean[], distances: number[]): number {
+    let idx = -1;
+    let lowestDistances = Infinity;
+
+    for (let i = 0; i < seen.length; ++i) {
+        if (seen[i]) {
+            continue;
+        }
+        if (lowestDistances > distances[i]) {
+            lowestDistances = distances[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+export default function dijkstra_list(
+    source: number,
+    sink: number,
+    arr: WeightedAdjacencyList,
+): number[] {
+    const seen = new Array(arr.length).fill(false);
+    const prev = new Array(arr.length).fill(-1);
+    const distances = new Array(arr.length).fill(Infinity);
+    distances[source] = 0;
+
+    while (hasUnvisited(seen, distances)) {
+        const curr = getLowestUnvisited(seen, distances);
+        seen[curr] = true;
+        const adjs = arr[curr];
+
+        for (let i = 0; i < adjs.length; ++i) {
+            const edge = adjs[i];
+            if (seen[edge.to]) {
+                continue;
+            }
+
+            const dist = distances[curr] + edge.weight;
+            if (dist < distances[edge.to]) {
+                distances[edge.to] = dist;
+                prev[edge.to] = curr;
+            }
+        }
+    }
+    const out: number[] = [];
+    let curr = sink;
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+    out.push(source);
+    return out.reverse();
+}
